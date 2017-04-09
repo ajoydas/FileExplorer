de91a5149acba78e1d651c7cc10ca1f1f05046eb
Shikkanobish <1405079.ad@ugrad.cse.buet.ac.bd>
2017-04-10 00:00:48 +0600

    Added tree view activity with current directory. bGo works and shows selected directory in tree view

--- src/Controller.java
+++ src/Controller.java
@@ -1,27 +1,40 @@
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
+import java.net.InetAddress;
 import java.net.URL;
+import java.net.UnknownHostException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 
 public class Controller implements Initializable {
     public TreeView treeView;
     public TableView tableView;
-    public TextField bCurrDir;
+    public TextField tCurrDir;
     public Button bGo;
     public MenuBar mMenu;
     public Button bBack;
     public Button bUp;
 
+    TreeItem<String> rootNode;
+    String hostName="computer";
+    ObservableList<FilePathTreeItem> drives;
     Image fileImage = new Image(
             getClass().getResourceAsStream("file-icon.png"));
     Image folderImage = new Image(
             getClass().getResourceAsStream("folder-icon.png"));
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        TreeItem<String> treeItemRoot = new TreeItem<> ("Root");
+        FilePathTreeItem.textField=tCurrDir;
+
+        /*TreeItem<String> treeItemRoot = new TreeItem<> ("Root");
 
         TreeItem<String> nodeItemA = new TreeItem<>("Item A");
         TreeItem<String> nodeItemB = new TreeItem<>("Item B");
@@ -34,14 +47,109 @@ public class Controller implements Initializable {
                 new ImageView(folderImage));
         TreeItem<String> nodeItemA3 = new TreeItem<>("Item A3",
                 new ImageView(fileImage));
+        TreeItem<String> nodeItemB1 = new TreeItem<>("Item A1",
+                new ImageView(folderImage));
+        TreeItem<String> nodeItemB2 = new TreeItem<>("Item A2",
+                new ImageView(folderImage));
+        TreeItem<String> nodeItemB3 = new TreeItem<>("Item A3",
+                new ImageView(fileImage));
 
         nodeItemA.getChildren().addAll(nodeItemA1, nodeItemA2, nodeItemA3);
+        nodeItemB.getChildren().addAll(nodeItemB1, nodeItemB2, nodeItemB3);
 
         //treeView= new TreeView<>(treeItemRoot);
         treeView.setRoot(treeItemRoot);
         treeView.setShowRoot(false);
+        treeView.getSelectionModel().select(nodeItemB2);*/
+
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
 
 
-        System.out.println("Added tree view");
+        try{hostName= InetAddress.getLocalHost().getHostName();}catch(UnknownHostException x){
+            x.printStackTrace();
+        }
+        tCurrDir.setText(System.getProperty("user.dir"));
+        drives=FXCollections.observableArrayList();
+        rootNode=new TreeItem<>(hostName,new ImageView(new Image(ClassLoader.getSystemResourceAsStream("folder-icon.png"))));
+        Iterable<Path> rootDirectories= FileSystems.getDefault().getRootDirectories();
+        for(Path name:rootDirectories){
+            FilePathTreeItem treeNode=new FilePathTreeItem(name.toFile());
+            drives.add(treeNode);
+            rootNode.getChildren().add(treeNode);
+        }
+
+        treeView.setRoot(rootNode);
+
+        treeView.getSelectionModel().select(getSpecificItem(System.getProperty("user.dir")));
+        treeView.scrollTo(treeView.getSelectionModel().getSelectedIndex());
+
+        /*for (FilePathTreeItem fileItem : FilePathTreeItem.allFolders){
+            System.out.println(fileItem.getAbsolutePath());
+            if(fileItem.getAbsolutePath().equals(tCurrDir.getText()))
+            {
+                treeView.getSelectionModel().select(fileItem);
+                break;
+            }
+        }*/
+
+        bGo.setOnMouseClicked(event ->
+                {
+                    FilePathTreeItem item = getSpecificItem(tCurrDir.getText());
+                    treeView.getSelectionModel().select(item);
+                    treeView.scrollTo(treeView.getSelectionModel().getSelectedIndex());
+                }
+        );
     }
+
+    private FilePathTreeItem getSpecificItem(String currDir) {
+        boolean isDrive=true;
+        StringTokenizer st = new StringTokenizer(currDir,"\\");
+        FilePathTreeItem fileItem=null;
+        String filename="";
+        while (st.hasMoreTokens()) {
+            filename+=st.nextToken();
+            System.out.println(filename);
+            if(isDrive)
+            {
+                filename+="\\";
+                isDrive=false;
+                //ObservableList<FilePathTreeItem> children= rootNode.getChildren();
+                for (FilePathTreeItem item: drives)
+                {
+                    //System.out.println(item.getFile().toString());
+                    if(item.getFile().toString().equals(filename))
+                    {
+                        fileItem=item;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                //fileItem.getChildren();
+                fileItem.setExpanded(true);
+                for (FilePathTreeItem item:fileItem.childrenArray)
+                {
+                    //System.out.println(item.getFile().toString());
+                    if(item.getFile().toString().equals(filename))
+                    {
+                        fileItem=item;
+                        break;
+                    }
+                }
+                filename+="\\";
+
+            }
+
+        }
+        tCurrDir.setText(fileItem.getFile().toString());
+        return fileItem;
+    }
+
+
+//    public void mouseClicked(MouseEvent mouseEvent) {
+//        TreeItem<String> item= (TreeItem<String>) treeView.getSelectionModel().getSelectedItem();
+//
+//    }
 }
---
+++ src/FilePathTreeItem.java
@@ -0,0 +1,118 @@
+import java.io.File;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class FilePathTreeItem extends TreeItem<String>{
+    public static TextField textField=null;
+    public static  ObservableList<FilePathTreeItem> allFolders=FXCollections.observableArrayList();
+    public  ObservableList<FilePathTreeItem> childrenArray=FXCollections.observableArrayList();
+
+    public static Image folderCollapseImage=new Image(ClassLoader.getSystemResourceAsStream("folder-icon.png"));
+    public static Image folderExpandImage=new Image(ClassLoader.getSystemResourceAsStream("folder-icon.png"));
+    public static Image fileImage=new Image(ClassLoader.getSystemResourceAsStream("file-icon.png"));
+    private boolean isLeaf;
+    private boolean isFirstTimeChildren=true;
+    private boolean isFirstTimeLeaf=true;
+    private final File file;
+    public File getFile(){return(this.file);}
+    private final String absolutePath;
+    public String getAbsolutePath(){return(this.absolutePath);}
+    private final boolean isDirectory;
+    public boolean isDirectory(){return(this.isDirectory);}
+
+    public FilePathTreeItem(File file){
+        super(file.toString());
+        this.file=file;
+        this.absolutePath=file.getAbsolutePath();
+        this.isDirectory=file.isDirectory();
+        if(this.isDirectory){
+            this.setGraphic(new ImageView(folderCollapseImage));
+            //add event handlers
+            this.addEventHandler(TreeItem.branchCollapsedEvent(),new EventHandler(){
+                @Override
+                public void handle(Event e){
+                    FilePathTreeItem source=(FilePathTreeItem)e.getSource();
+                    System.out.println(source.absolutePath);
+                    textField.setText(source.absolutePath);
+                    if(!source.isExpanded()){
+                        ImageView iv=(ImageView)source.getGraphic();
+                        iv.setImage(folderCollapseImage);
+                    }
+                }
+            } );
+            this.addEventHandler(TreeItem.branchExpandedEvent(),new EventHandler(){
+                @Override
+                public void handle(Event e){
+                    FilePathTreeItem source=(FilePathTreeItem)e.getSource();
+                    System.out.println(source.absolutePath);
+                    textField.setText(source.absolutePath);
+                    if(source.isExpanded()){
+                        ImageView iv=(ImageView)source.getGraphic();
+                        iv.setImage(folderExpandImage);
+                    }
+                }
+            } );
+
+
+        }else{
+            this.setGraphic(new ImageView(fileImage));
+        }
+        //set the value (which is what is displayed in the tree)
+        String fullPath=file.getAbsolutePath();
+        if(!fullPath.endsWith(File.separator)){
+            String value=file.toString();
+            int indexOf=value.lastIndexOf(File.separator);
+            if(indexOf>0){
+                this.setValue(value.substring(indexOf+1));
+            }else{
+                this.setValue(value);
+            }
+        }
+    }
+
+    @Override
+    public ObservableList<TreeItem<String>> getChildren(){
+        System.out.println("Getting Children for "+file.toString());
+        if(isFirstTimeChildren){
+            isFirstTimeChildren=false;
+            super.getChildren().setAll(buildChildren(this));
+        }
+        return(super.getChildren());
+    }
+
+    @Override
+    public boolean isLeaf(){
+        if(isFirstTimeLeaf){
+            isFirstTimeLeaf=false;
+            isLeaf=this.file.isFile();
+        }
+        return(isLeaf);
+    }
+
+    private ObservableList<FilePathTreeItem> buildChildren(FilePathTreeItem treeItem){
+        System.out.println("Building Children for "+file.toString());
+        File f=treeItem.getFile();
+        if((f!=null)&&(f.isDirectory())){
+            File[] files=f.listFiles();
+            if (files!=null){
+                ObservableList<FilePathTreeItem> children=FXCollections.observableArrayList();
+                childrenArray=FXCollections.observableArrayList();
+                for(File childFile:files){
+                    FilePathTreeItem item= new FilePathTreeItem(childFile);
+                    children.add(item);
+                    //System.out.println("Adding child");
+                    childrenArray.add(item);
+                }
+                return(children);
+            }
+        }
+        return FXCollections.emptyObservableList();
+    }
+
+}
